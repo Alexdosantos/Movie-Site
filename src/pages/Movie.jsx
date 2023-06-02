83d788f60c77caeb010796b1963f3ec8603b76f3
Alex dos Santos <125704045+Alexdosantos@users.noreply.github.com>
2023-06-02 00:55:51 -0300

    finalizado

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -12,14 +12,16 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const Movie =() => {
     const {id} = useParams()
     const [movie, setMovie] = useState(null)
-
-    const getMovie = async(url)  =>  {
-        const res = await fetch(url)
-        const data = await res.json()
-        console.log(data);
     
-       setMovie(data)
+    const getMovie = async(url) => {
+        const response = await fetch(url)
+        const data = await response.json()
+        setMovie(data)
     }
+
+
+
+    
 
     useEffect(() => {
         const movieUrl = `${moviesURL}${id}?${apiKey}`;
